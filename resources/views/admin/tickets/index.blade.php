a0fc3287da8719b0889078cb40056af68cca44bf
ilham desqi <164368396+ilhamdesqidev@users.noreply.github.com>
2025-09-25 09:34:17 +0700

    halaman tiket

---
+++ resources/views/admin/tickets/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Tiket Settings')
+@section('page-title', 'Tiket Settings')
+@section('page-description', 'Manage your application tickets here.')
+
+@section('content')
+<h1>haha</h1>
+@endsection
