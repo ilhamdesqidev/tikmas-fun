134d824f1eac02901ad1b2c386deca4a7b7a29cf
FATTFAWW <164737174+FATTFAWW@users.noreply.github.com>
2025-09-27 14:09:19 +0700

    revisi tersulit: luwas to lunas goddammn

--- resources/views/payment/invoice-pdf.blade.php
+++ resources/views/payment/invoice-pdf.blade.php
@@ -383,7 +383,7 @@
                 <div class="total-label">Total Pembayaran</div>
                 <div>
                     <span class="total-amount">Rp {{ number_format($order->total_price, 0, ',', '.') }}</span>
-                    <span class="status-badge">LUWAS</span>
+                    <span class="status-badge">LUNAS</span>
                 </div>
             </div>
             
