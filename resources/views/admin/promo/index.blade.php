23e3f3c33f007a18524646ae34013a44ba552cb5
ilham desqi <164368396+ilhamdesqidev@users.noreply.github.com>
2025-10-02 10:31:01 +0700

    hapus ubah status

--- resources/views/admin/promo/index.blade.php
+++ resources/views/admin/promo/index.blade.php
@@ -170,13 +170,6 @@
                         {{ ucfirst($promo->category) }}
                     </span>
                     <div class="flex space-x-2">
-                        <!-- Toggle Status Button -->
-                        <button onclick="togglePromoStatus({{ $promo->id }})" class="p-2 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors duration-200 flex items-center justify-center w-9 h-9" title="Ubah Status">
-                            <svg class="w-4 h-4 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"></path>
-                            </svg>
-                        </button>
-                        
                         <a href="{{ route('admin.promo.edit', $promo->id) }}" class="p-2 bg-gray-100 rounded-lg hover:bg-gray-200 transition-colors duration-200 flex items-center justify-center w-9 h-9" title="Edit">
                             <svg class="w-4 h-4 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path>
