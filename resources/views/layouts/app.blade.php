41e72432635d68e3a355c4117d08b14d71e08cac
FATTFAWW <164737174+FATTFAWW@users.noreply.github.com>
2025-10-29 10:29:24 +0700

    icon voucher

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -169,10 +169,10 @@
                     </a>
                 </li>
 
-                <li>
+               <li>
                     <a href="{{ route('admin.voucher.index') }}" class="nav-link flex items-center px-4 py-3 text-gray-300 rounded-lg hover:bg-sidebar-hover hover:text-white transition-all duration-200">
                         <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"></path>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"></path>
                         </svg>
                         Voucher
                     </a>
